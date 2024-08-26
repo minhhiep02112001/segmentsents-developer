e59740ecb713404ffa51c40750425b6b798bc354
@hiepcm <hiepcm@imap.edu.vn>
2024-08-26 10:11:02 +0700

    update code

--- src/resources/views/widgets/segment/filter.blade.php
+++ src/resources/views/widgets/segment/filter.blade.php
@@ -7,12 +7,27 @@
                 <option value="">Chọn Segment</option>
                 @foreach ($segments as $segment)
                     @php
-                        $url = !empty($route_name)
-                            ? route(\Config::get('route.as') . $route_name, [
+                        $url = '/';
+                        if (!empty($route_name)) {
+                            // Lấy route hiện tại
+                            $currentRoute = request()->route();
+                            // Lấy pattern của URL hiện tại
+                            $urlPattern = $currentRoute->uri();
+
+                            // Tạo tham số mặc định cho route
+                            $routeParams = [
                                 'filter' => json_decode(json_decode($segment['data'])->filter, true) ?? '',
-                            ])
-                            : '/';
+                            ];
+
+                            // Kiểm tra nếu pattern có chứa '{type}'
+                            if (strpos($urlPattern, '{type}') !== false) {
+                                // Thêm 'type' vào tham số nếu cần
+                                $routeParams['type'] = $type ?? '';
+                            }
+                            $url = route(\Config::get('route.as') . $route_name, $routeParams);
+                        } 
                     @endphp
+  
                     <option value="{{ $url }}">
                         {{ $segment['name'] ?? '' }}</option>
                 @endforeach
